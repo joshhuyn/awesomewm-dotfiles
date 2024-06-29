759c8ee6d2b0e561eac1b4db30c4b7b1bae3bfff
joshhuyn <josh-it@outlook.com>
2024-06-29 15:21:31 +0200

    update mouse lock

--- rc.lua
+++ rc.lua
@@ -11,17 +11,9 @@ local gears = require("gears")
 local beautiful = require("beautiful")
 local configHandler = require("configurators/ConfigHandler"):setup()
 
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", function (s) configHandler:setWallpaper(s) end)
-
-client.connect_signal("manage", function (c)
-    if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
-        awful.placement.no_offscreen(c)
-    end
-
-    c:connect_signal("mouse::leave", function(c)
+function handle_mouse_lock(c)
         if IS_MOUSE_LOCKED then
-            local margin = 5
+            local margin = 0
             local cg = c:geometry()
             local mg = mouse.coords()
 
@@ -43,7 +35,15 @@ client.connect_signal("manage", function (c)
 
             mouse.coords({ x = newx, y = newy })
         end
-    end)
+end
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", function (s) configHandler:setWallpaper(s) end)
+
+client.connect_signal("manage", function (c)
+    if awesome.startup and not c.size_hints.user_position and not c.size_hints.program_position then
+        awful.placement.no_offscreen(c)
+    end
 end)
 
 client.connect_signal("focus", function(c)
@@ -52,11 +52,16 @@ client.connect_signal("focus", function(c)
     end
     c.border_color = beautiful.border_focus
     c.border_width = beautiful.border_width
+
+    c:connect_signal("mouse::leave", handle_mouse_lock)
 end)
+
 client.connect_signal("unfocus", function(c)
     c.shape = function(cr, width, height)
         gears.shape.rounded_rect(cr, width, height)
     end
     c.border_color = beautiful.border_normal
     c.border_width = beautiful.border_width
+
+    c:disconnect_signal("mouse::leave", handle_mouse_lock)
 end)
